4f207388aad02b505c262604f0ce47ef1450a27b
clearlyyy <thorntonjaiden5@gmail.com>
2022-12-24 18:52:27 -0800

    Blob, insteead of a torus knot!

--- src/Pages/home-page/Home.js
+++ src/Pages/home-page/Home.js
@@ -4,6 +4,7 @@ import {Canvas} from "@react-three/fiber";
 import {OrbitControls} from "@react-three/drei"
 import Box from "./Box"
 import Torus from "./Torus"
+import Blob from "./Blob"
 
 const Home = () => {
   return (
@@ -21,7 +22,7 @@ const Home = () => {
         <OrbitControls enableZoom={false}/>
         <ambientLight intensity={0.5}/>
         <directionalLight position={[-2,5,2]} intesity={1}/>
-        <Torus/>
+        <Blob/>
       </Canvas>
       </div>
       
